f72df6ba27ad1e08ef8db95b71c457e3252f2f77
Chris Anderson <cnanders@gmail.com>
2019-09-19 17:04:12 -0700

    updated conv to support matrix style pupil data

--- src/PT_conv_src.m
+++ src/PT_conv_src.m
@@ -1,47 +1,67 @@
 
 
-% Convolves two pupil fill srcs together and returns the output with a
+% Returns the convolution of two pupil fill srcs resampled to a
 % resolution of pixels (last param)
-% @param {struct} pupil1
-% @param {double 1xm} pupil1.x - list of sigmaX values of src1
-% @param {double 1xm} pupil1.y - list of sigmaY values of src1
-% @param {double 1xm} pupil1.z - list of relative intensity values of src1
-% @param {struct} pupil1
-% @param {double 1xm} pupil2.x - list of sigmaX values of src2
-% @param {double 1xm} pupil2.y - list of sigmaY values of src2
-% @param {double 1xm} pupil2.z - list of relative intensity values of src2
+% @param {struct 1x1} pupil1 - a structure storing the x and y coordinates as well
+% as the corresponding intensities in the .x, .y and .z components of the
+% structure. The size and shapes of each of these is identical and can be
+% either a one-dimensional list (just specifying spots) or two-dimensional
+% matrices (bitmaps).
+% @param {struct 1x1} pupil2 - see pupil1 definition
 % @param {double 1x1} dPixels - number of pixels in output pupil
-% @return {struct} pupil
-% @return {double pixels * pixels} pupil.x - sigmaX values of output
-% @return {double pixels * pixels} pupil.y - sigmaY values of output
-% @return {double pixels * pixels} pupil.z - intensity values of output
+% @return {struct 1x1} pupil - see pupil1 definition
 
 function pupil = PT_conv_src(pupil1, pupil2, dPixels)
 
+% Output pupil sampling
 dX = linspace(-1, 1, dPixels);
 dY = dX;
 [dX, dY] = meshgrid(dX, dY);
 
+% If pupil1 data is 2D, convert to 1D list for interpolation with griddata
+[dRows, dCols] = size(pupil1.x);
+if (dRows == 1 || dCols == 1)
+    dX1 = pupil1.x;
+    dY1 = pupil1.y;
+    dZ1 = pupil1.z;
+else
+    dX1 = reshape(pupil1.x, [1, dRows * dCols]);
+    dY1 = reshape(pupil1.y, [1, dRows * dCols]);
+    dZ1 = reshape(pupil1.z, [1, dRows * dCols]);
+end
+    
+% Interpolate pupil1 on output grid
 dZ1Cart = griddata(...
-    pupil1.x, pupil1.y, pupil1.z, ...
+    dX1, dY1, dZ1, ...
     dX, ...
     dY ...
 );
 
+
+% If pupil2 data is 2D, convert to 1D list for interpolation with griddata
+[dRows, dCols] = size(pupil2.x);
+if (dRows == 1 || dCols == 1)
+    dX2 = pupil2.x;
+    dY2 = pupil2.y;
+    dZ2 = pupil2.z;
+else
+    dX2 = reshape(pupil2.x, [1, dRows * dCols]);
+    dY2 = reshape(pupil2.y, [1, dRows * dCols]);
+    dZ2 = reshape(pupil2.z, [1, dRows * dCols]);
+end
+
+% Interpolate pupil2 on output grid
 dZ2Cart = griddata(...
-    pupil2.x, pupil2.y, pupil2.z, ...
+    dX2, dY2, dZ2, ...
     dX, ...
     dY ...
 );
-
-% dZ1Cart = dZ1Cart ./ max(max(dZ1Cart));
-% dI2Cart = dI2Cart ./ max(max(dI2Cart));
 
 dZ = conv2(dZ1Cart, dZ2Cart, 'same');
 dZ = dZ ./ max(max(dZ)); 
 
-pupil.x = dX;
-pupil.y = dY;
-pupil.z = dZ;
+pupil.x = reshape(dX, [1, dPixels * dPixels]);
+pupil.y = reshape(dY, [1, dPixels * dPixels]);
+pupil.z = reshape(dZ, [1, dPixels * dPixels]);
 
 end
--- test/test_PT_conv_src.m
+++ test/test_PT_conv_src.m
@@ -18,5 +18,8 @@ addpath(cDirMpm);
 p1 = PT_read_src();
 p2 = PT_read_src();
 p = PT_conv_src(p1, p2, 251);
+PT_display(p)
 
+p3 = PT_read_src();
+p = PT_conv_src(p, p3, 51);
 PT_display(p)
